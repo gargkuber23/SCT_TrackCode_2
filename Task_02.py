58ce5df441a47cda2356bf6805f163d479f7fa48
gargkuber23 <kubergarg822@gmail.com>
2025-03-20 12:23:10 +0530

    Add files via upload

---
+++ Task_02.py
@@ -0,0 +1,24 @@
+#Strength of password
+import re
+def assess_password_strength(password):
+    length_criteria = len(password) >= 8
+    uppercase_criteria = re.search(r'[A-Z]', password) is not None
+    lowercase_criteria = re.search(r'[a-z]', password) is not None
+    number_criteria = re.search(r'[0-9]', password) is not None
+    special_char_criteria = re.search(r'[@$!%*?&]', password) is not None
+    criteria_met = sum([length_criteria, uppercase_criteria, lowercase_criteria,
+                        number_criteria, special_char_criteria])
+    if criteria_met == 5:
+        strength = "Very Strong"
+    elif criteria_met == 4:
+        strength = "Strong"
+    elif criteria_met == 3:
+        strength = "Moderate"
+    elif criteria_met == 2:
+        strength = "Weak"
+    else:
+        strength = "Very Weak"
+    return strength
+password = input("Enter a password to assess its strength: ")
+strength = assess_password_strength(password)
+print(f"Password Strength: {strength}")
